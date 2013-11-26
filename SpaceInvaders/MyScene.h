06b9934fff6e4509e971740868a2ff9ce0e5de0c
Mohamed El sherif <mohamedalsherif@hotmail.com>
2013-11-26 23:31:03 +0100

    Made the project Sprite Kit compatible

---
+++ SpaceInvaders/MyScene.h
@@ -0,0 +1,13 @@
+//
+//  MyScene.h
+//  SpaceInvadersIOS
+//
+
+//  Copyright (c) 2013 M. All rights reserved.
+//
+
+#import <SpriteKit/SpriteKit.h>
+
+@interface MyScene : SKScene
+
+@end
--- SpaceInvaders/ViewController.m
+++ SpaceInvaders/ViewController.m
@@ -6,27 +6,48 @@
 //  Copyright (c) 2013 Mohamed Emad. All rights reserved.
 //
 
+
 #import "ViewController.h"
-
-@interface ViewController ()
-
-@end
+#import "MyScene.h"
 
 @implementation ViewController
 
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
+    
+    // Configure the view.
+    SKView * skView = (SKView *)self.view;
+    skView.showsFPS = YES;
+    skView.showsNodeCount = YES;
+    
+    // Create and configure the scene.
+    SKScene * scene = [MyScene sceneWithSize:skView.bounds.size];
+    scene.scaleMode = SKSceneScaleModeAspectFill;
+    
+    // Present the scene.
+    [skView presentScene:scene];
 }
 
+- (BOOL)shouldAutorotate
+{
+    return YES;
+}
+
+- (NSUInteger)supportedInterfaceOrientations
+{
+    if ([[UIDevice currentDevice] userInterfaceIdiom] == UIUserInterfaceIdiomPhone) {
+        return UIInterfaceOrientationMaskAllButUpsideDown;
+    } else {
+        return UIInterfaceOrientationMaskAll;
+    }
+}
 
 - (void)didReceiveMemoryWarning
 {
     [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+    // Release any cached data, images, etc that aren't in use.
 }
 
-
-
 @end
+
