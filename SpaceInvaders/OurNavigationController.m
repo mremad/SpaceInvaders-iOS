b194ddd135d5cb7883779ec2065025a8ff03fd7a
Mohamed El sherif <mohamedalsherif@hotmail.com>
2013-12-09 05:49:57 +0100

    added custom navigation controller

---
+++ SpaceInvaders/OurNavigationController.m
@@ -0,0 +1,63 @@
+//
+//  OurNavigationController.m
+//  SpaceInvaders
+//
+//  Created by M on 12/9/13.
+//  Copyright (c) 2013 M. All rights reserved.
+//
+
+#import "OurNavigationController.h"
+
+@interface OurNavigationController ()
+
+@end
+
+@implementation OurNavigationController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    
+    UIViewController *rootViewController = self.viewControllers[0];
+    if([self.visibleViewController isEqual:rootViewController])
+    {
+        [self setNavigationBarHidden:YES animated:NO];
+    }
+	// Do any additional setup after loading the view.
+}
+
+-(void) viewDidLayoutSubviews
+{
+    [super viewDidLayoutSubviews];
+    
+    UIViewController *rootViewController = self.viewControllers[0];
+    if([self.visibleViewController isEqual:rootViewController])
+    {
+        [self setNavigationBarHidden:YES animated:NO];
+    }
+    else
+    {
+        [self setNavigationBarHidden:NO animated:NO];
+    }
+    
+}
+
+
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
